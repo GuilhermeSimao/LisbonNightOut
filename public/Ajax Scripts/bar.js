926810cbcb6263ab0e79aea1a4bef3b734255ca0
guilherme simao <simaogui20@gmail.com>
2021-02-24 05:54:30 +0000

    bar page

--- public/Ajax Scripts/bar.js
+++ public/Ajax Scripts/bar.js
@@ -14,12 +14,26 @@ $(function(){
         success: function(bars) {
             
             barTitle = $('.bar-title');
+            descriptionText = $('#description-p');
+            addressText = $('#address');
+            contactText = $('#contact');
+            scheduleText = $('#schedule');
 
             barTitle.empty();
+            descriptionText.empty();
+            addressText.empty();
+            contactText.empty();
+            scheduleText.empty();
+
 
             $.each(bars,(i, bar)=>{
                 barTitle.append(bar.Name);
+                descriptionText.append(bar.Description);
+                addressText.append(bar.Address);
+                contactText.append(bar.Contacto);
+                scheduleText.append(bar.horario);
                 console.log(bar.Name);
+                $("#barImg").attr("src", bar.PictureURL);
             });
 
             
